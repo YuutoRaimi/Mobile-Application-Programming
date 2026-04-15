1e9ab2481cff913954f0493ac28d205b4bf88bb7
Yehuda <playzyehuda@gmail.com>
2026-04-15 18:20:38 +0700

    week 5

---
+++ README.md
@@ -0,0 +1,4 @@
+<img width="429" height="1080" alt="Commit No  2 - Parse JSON with Moshi and Display Image URL" src="https://github.com/user-attachments/assets/be6a8f99-2bf7-44f3-a94d-df3ff5e23e35" />
+<img width="431" height="1080" alt="Commit No  1 - Retrofit Setup and Fetch API JSON Response" src="https://github.com/user-attachments/assets/ce639e39-2a6c-4de0-8db8-d5745ec5c94b" />
+<img width="426" height="1080" alt="Commit assignment breed" src="https://github.com/user-attachments/assets/7a4e1709-a8ed-4ab1-8497-7f8fd25db39c" />
+<img width="431" height="1080" alt="Commit No  3 - Load and Display Cat Image using Glide" src="https://github.com/user-attachments/assets/1e55fcaa-8ee9-4465-960f-1a81efe5dc97" />
---
+++ app/src/main/java/com/example/lab_week_05/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.lab_week_05
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lab_week_05.api.CatApiService
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import com.example.lab_week_05.model.ImageData
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    companion object { const val MAIN_ACTIVITY = "MAIN_ACTIVITY" }
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://api.thecatapi.com/v1/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    private val catApiService by lazy {
+        retrofit.create(CatApiService::class.java)
+    }
+
+    private val apiResponseView: TextView by lazy { findViewById(R.id.api_response) }
+    private val imageResultView: ImageView by lazy { findViewById(R.id.image_result) }
+    private val imageLoader: ImageLoader by lazy { GlideLoader(this) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        getCatImageResponse()
+    }
+
+    private fun getCatImageResponse() {
+        val call = catApiService.searchImages(1, "full")
+        call.enqueue(object: Callback<List<ImageData>> {
+            override fun onFailure(call: Call<List<ImageData>>, t: Throwable) {
+                Log.e(MAIN_ACTIVITY, "Failed to get response", t)
+            }
+
+            override fun onResponse(call: Call<List<ImageData>>, response: Response<List<ImageData>>) {
+                if (response.isSuccessful) {
+                    val first = response.body()?.firstOrNull()
+                    val imageUrl = first?.imageUrl.orEmpty()
+                    val breedName = first?.breeds?.firstOrNull()?.name ?: "Unknown"
+
+                    apiResponseView.text = "Breed: $breedName"
+
+                    if (imageUrl.isNotBlank()) {
+                        imageLoader.loadImage(imageUrl, imageResultView)
+                    } else {
+                        Log.d(MAIN_ACTIVITY, "Missing image URL")
+                    }
+                } else {
+                    Log.e(MAIN_ACTIVITY, "Failed to get response\n" + response.errorBody()?.string().orEmpty())
+                }
+            }
+        })
+    }
+}
